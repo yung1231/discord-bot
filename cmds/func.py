e7f7733534623dbb403695e25629cf2726044d76
yung1231 <pcymichael@gmail.com>
2022-12-21 15:44:39 +0800

    add cat

--- cmds/func.py
+++ cmds/func.py
@@ -4,7 +4,21 @@ from core.classes import Cog_Extension
 import requests
 
 class Func(Cog_Extension):
-  pass
+  @commands.command()
+  async def cat(self, ctx):
+    # fi = discord.File('./1.jpg')
+    # await ctx.send(file=fi)
+    
+    # response = requests.get('https://some-random-api.ml/img/cat')
+    response = requests.get('https://aws.random.cat/meow')
+    data = response.json()
+    embed = discord.Embed(
+      title = 'Cat 🐈',
+      description = ':star_struck: :star_struck: :star_struck:',
+      colour = discord.Colour.purple())
+    embed.set_image(url=data['file'])            
+    embed.set_footer(text="")
+    await ctx.send(embed=embed)
 
 # 運行 bot.py 就會執行 setup
 # 讓 bot 註冊我們所寫的類別
